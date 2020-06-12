25d533faa95c883fc8f93160e7d1808b5302e8fd
tong tong <feraltech.ray@gmail.com>
2020-06-12 11:16:11 +0800

    Commit Rectify

--- src/configFirebase.js
+++ src/configFirebase.js
@@ -3,7 +3,16 @@ import 'firebase/firestore';
 import 'firebase/messaging';
 import 'firebase/storage';
 
-const firebaseConfig = require('./firebaseKey');
+const firebaseConfig = {
+  apiKey: "AIzaSyBQoIem21sdLQPsNiSP54zeUQ77vrXHMrw",
+  authDomain: "cropchien-75aaa.firebaseapp.com",
+  databaseURL: "https://cropchien-75aaa.firebaseio.com",
+  projectId: "cropchien-75aaa",
+  storageBucket: "cropchien-75aaa.appspot.com",
+  messagingSenderId: "888004066775",
+  appId: "1:888004066775:web:af4f4469f42956ba409f92",
+  measurementId: "G-KQ6YR9208H"
+};
 
 firebase.initializeApp(firebaseConfig)
 
